47cca9b1bbdd430d2ce31411f8eed70bbcdf8dca
AlexeyKomyshev <aleksejkomysev36@gmail.com>
2025-01-19 12:13:18 +0300

    configuration

---
+++ config/storybook/preview.js
@@ -0,0 +1,33 @@
+import { addDecorator } from '@storybook/react';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenceDecorator/SuspenceDecorator';
+import { Theme } from '../../src/shared/const/theme';
+import 'loki/configure-react';
+import { FeatureFlagsDecorator } from '../../src/shared/config/storybook/FeatureFlagsDecorator/FeatureFlagsDecorator';
+
+export const parameters = {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+    layout: 'fullscreen',
+    themes: {
+        default: 'light',
+        list: [
+            { name: 'light', class: Theme.LIGHT, color: '#ffffff' },
+            { name: 'dark', class: Theme.DARK, color: '#101535' },
+            { name: 'orange', class: Theme.ORANGE, color: '#df4f1a' },
+        ],
+    },
+};
+
+addDecorator(StyleDecorator);
+addDecorator(ThemeDecorator(Theme.LIGHT));
+addDecorator(RouterDecorator);
+addDecorator(SuspenseDecorator);
+addDecorator(FeatureFlagsDecorator({}));
